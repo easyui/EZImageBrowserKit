9f0248fb922f99230a010c1d1046ab45d6a1702a
easyui <gzhuyangjun@gmail.com>
2017-06-30 20:10:55 +0800

    add example

---
+++ EZImageBrowserKitExample/EZImageBrowserKitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EZImageBrowserKitExample
+//
+//  Created by IQIYI on 2017/6/30.
+//  Copyright © 2017年 Zhu yangjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EZImageBrowserKitExample/EZImageBrowserKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZImageBrowserKitExample
+//
+//  Created by IQIYI on 2017/6/30.
+//  Copyright © 2017年 Zhu yangjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
